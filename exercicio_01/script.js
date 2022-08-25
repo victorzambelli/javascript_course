4d6b2a8e61918fb63097708b54f66765e1a6dc56
Victor Z <strkergames@gmail.com>
2022-08-25 00:50:08 -0300

    First Exercise
    Using Variables, Prompt, Alert.

---
+++ exercicio_01/script.js
@@ -0,0 +1,7 @@
+alert('Bem-vindo! A seguir pediremos que informe alguns dados.');
+
+let nome = prompt('Informe seu nome:');
+let age = prompt('Informe sua idade:');
+let confirmAge = prompt('Confirme sua idade:');
+
+alert(`Nome Inserido: ${nome} \nIdade Inserida: ${age} \nConfirmação de Idade: ${confirmAge}`);
